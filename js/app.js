1701c5b315e6d39c4ea7b006b0f2e2f82f10c45c
Edith Mandujano <edithmandujano001@gmail.com>
2017-05-29 16:48:10 -0500

    intento de que funcione next prev

--- js/app.js
+++ js/app.js
@@ -20,4 +20,39 @@ $(document).ready(function(){
     $(".image").hide();
     $("#img5").show();
   });
+
+if ($("#img1").show()) {
+  $('#anterior').click(function(){
+    $(".image").hide();
+    $("#img5").show();
+  });
+}
+if ($("#img2").show()) {
+  $('#anterior').click(function(){
+    $(".image").hide();
+    $("#img1").show();
+  });
+}
+if ($("#img3").show()) {
+  $('#anterior').click(function(){
+    $(".image").hide();
+    $("#img2").show();
+  });
+}
+if ($("#img4").show()) {
+  $('#anterior').click(function(){
+    $(".image").hide();
+    $("#img3").show();
+  });
+}
+if ($("#img5").show()) {
+  $('#anterior').click(function(){
+    $(".image").hide();
+    $("#img4").show();
+  });
+}
+
+
+
+
 });
